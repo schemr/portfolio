7ee5a19a53f568dfdf43d1579fd32104402e074f
schemr <schemryj@gmail.com>
2014-11-03 18:16:47 +0900

    v0.4
    angularjs / layout / blog basic finish

--- script/app.js
+++ script/app.js
@@ -1,33 +1,109 @@
-var app = angular.module('ngPortfolio', [
-  'ngRoute',
-  'ngAnimate'
-]);
+var app = angular.module('ngPortfolio', ['ngRoute','ngAnimate']);
 
 app.config(function ($routeProvider) {
   $routeProvider
-    .when('/', {
-        templateUrl: 'script/portfolio/templates/page-home.html',
-        controller: 'mainController'
+    .when('/home', {
+        templateUrl: 'script/templates/page-home.html',
+        controller: 'homeController'
     })
     .when('/about', {
-        templateUrl: 'script/portfolio/templates/page-about.html',
+        templateUrl: 'script/templates/page-about.html',
         controller: 'aboutController'
     })
-    .when('/contact', {
-        templateUrl: 'page-contact.html',
-        controller: 'contactController'
+    .when('/portfolio', {
+        templateUrl: 'script/templates/page-portfolio.html',
+        controller: 'portfolioController'
     })
-   .otherwise({redirectTo: '/'});
+    .when('/blog', {
+        templateUrl: 'script/templates/page-blog.html',
+        controller: 'blogController'
+    })
+    .when('/timeline', {
+        templateUrl: 'script/templates/page-timeline.html',
+        controller: 'timelineController'
+    })
+   .otherwise({redirectTo: '/home'});
  });
 
-app.controller('mainController', function($scope) {
-    $scope.pageClass = 'page-home';
+app.controller('headerCtrl',function($scope){
+    $scope.bgStyle = {
+        backgroundColor:'#eeeeee',
+        color:'#9e9e9e'
+    };
+    $scope.header = {
+        img:"images/schemr.jpg",
+        imgAlt:"logo",
+        title:"SCHEMR"
+    };
 });
+
+app.controller('homeController', function($scope, $anchorScroll) {
+    $scope.pageClass = 'page-home';
+    $scope.bgStyle.backgroundColor = "#eeeeee";
+    $scope.bgStyle.color = "#9e9e9e";
+    $scope.header.img = 'images/schemr.jpg';
+    $scope.header.title = "SCHEMR";
+    var menuLayout = function(){
+      var pageHeight = (window.innerHeight-140)/2;
+      $(".ripplelink").css("height",pageHeight);
+    };
+    menuLayout();
+});
+
+app.controller('footerCtrl', ['$scope', '$location', '$anchorScroll',
+    function ($scope, $location, $anchorScroll) {
+      $scope.gotoTop = function() {
+        // set the location.hash to the id of
+        // the element you wish to scroll to.
+        $location.hash('top');
+
+        // call $anchorScroll()
+        $anchorScroll();
+      };
+}]);
 
 app.controller('aboutController', function($scope) {
-    $scope.pageClass = 'page-about';
+    $scope.pageClass = 'page-sub';
+    $scope.bgStyle.backgroundColor = "#00bcd4";
+    $scope.bgStyle.color = "#fff";
+    $scope.header.img = "images/left.png";
+    $scope.header.imgAlt = "Go Back";
+    $scope.header.title = "ABOUT ME";
 });
 
-app.controller('contactController', function($scope) {
-    $scope.pageClass = 'page-contact';
+app.controller('portfolioController', function($scope) {
+    $scope.pageClass = 'page-sub';
+    $scope.bgStyle.backgroundColor = "#8bc34a";
+    $scope.bgStyle.color = "#fff";
+    $scope.header.img = "images/left.png";
+    $scope.header.imgAlt = "Go Back";
+    $scope.header.title = "PORTFOLIO";
+    var portfolioList = [
+        { name:"테스트", 
+          thumb:"neca.jpg",
+          alt:"한국보건의료연구원",
+          spec:"HTML5",
+          work:"프론트",
+          detail:"프론트앤드 개발",
+          percent:"퍼블리싱, UX, 스크립트 100%" 
+        }]
+    $scope.portfolioList = portfolioList;
+});
+
+app.controller('blogController', function($scope) {
+    $scope.pageClass = 'page-sub';
+    $scope.bgStyle.backgroundColor = "#ffc107";
+    $scope.bgStyle.color = "#fff";
+    $scope.header.img = "images/left.png";
+    $scope.header.imgAlt = "Go Back";
+    $scope.header.title = "BLOG POST";
+});
+
+app.controller('timelineController', function($scope) {
+    $scope.pageClass = 'page-sub';
+    $scope.bgStyle.backgroundColor = "#ff9800";
+    $scope.bgStyle.color = "#fff";
+    $scope.header.img = "images/left.png";
+    $scope.header.imgAlt = "Go Back";
+    $scope.header.title = "TIMELINE";
 });
